b45d136e867df9f61ad1e606222b9b0f091ae0e6
PedroHenriqueCoder <pedrohenrqueppontes3000@outlook.com>
2025-01-08 20:54:06 -0300

    Swagger/Documentaç~ Implementado

--- MasterPdv/src/main/java/com/masterPdv/MasterPdv/config/SwaggerConfig.java
+++ MasterPdv/src/main/java/com/masterPdv/MasterPdv/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class SwaggerConfig extends OpenAPI {
@@ -13,7 +15,16 @@ public class SwaggerConfig extends OpenAPI {
 	public OpenAPI customizeSwagger() {
 		return new OpenAPI()
 				  .components(new Components())
-				;
+				  .info(new Info()
+						  .title("MasterPDV - Ponto de Vendas Ultra Dinâmico V0.1")
+						  .description("Documentação da API para o sistema Master PDV")
+	                      .version("1.0.0")
+	                      .license(new License()
+	                                .name("Apache 2.0")
+	                                .url("http://springdoc.org")));
+
+						  
+
 	}
 	
 }
